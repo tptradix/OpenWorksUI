0ff63703ccab0465611edda65c443a93bab72ce5
tptradix <157167631+tptradix@users.noreply.github.com>
2024-04-29 21:24:19 +0300

    Update main.js

--- @app/src/js/main.js
+++ @app/src/js/main.js
@@ -1,211 +1,157 @@
-!function() {
-    document.body.onselectstart = function() { return false; }
-    const chx = ["modules", "cash", "root"];
-    const list = ["Android", "iPhone", "Safari", "iPad", "Chrome", "Opera"];
-    const modal = document.querySelector('.modals');
-    const closeBtn = document.querySelector('.modal__btn');
-    const mobileClose = document.querySelector('.mobile-close');
-    const exit = document.querySelector(".form__btn");
-    let isCrypto = document.getElementById('cp');
-    let onev = document.querySelector('.one_v_l');
-    let ch1 = document.getElementById('i_pc');
-    let form_c = document.querySelector('.c_form');
-    let cp_amount = document.getElementById('cp_amount');
-    let cryptobot = document.querySelector('.cp_pay');
-    let cryptobotb = document.getElementById('cp');
-    const openBtn = document.querySelector('.acp');
-    const openBtns = document.querySelectorAll('[id^=iws]');
-    const openBtnLs = document.querySelectorAll('.acp-l');
-    const wsts = document.querySelectorAll('[id^=infT]');
-    const iws = document.querySelectorAll('[id^=iws]');
-    const tws = document.querySelectorAll('[id^=tws]');
-    const pws = document.querySelectorAll('[id^=pws]');
-    const drows = document.querySelectorAll('[id^=row]');
-    
+$(function() {
+    const modal = $('.modals');
+    const closeBtn = $('.modal__btn');
+    const mobileClose = $('.mobile-close');
+    const cryptobotb = $('#cp');
+    const cp_amount = $('#cp_amount');
+    const cryptobot = $('.cp_pay');
+    const openBtn = $('.acp');
+    const openBtns = $('[id^=iws]');
+    const wsts = $('[id^=infT]');
+    const iws = $('[id^=iws]');
+    const tws = $('[id^=tws]');
+    const pws = $('[id^=pws]');
+    const drows = $('[id^=row]');
+
     const disableScroll = () => {
-        let pagePosition = window.scrollY;
-        document.body.classList.add('disable-scroll');
-        document.body.dataset.position = pagePosition;
-        document.body.style.top = -pagePosition + 'px';
-    }
+        const pagePosition = window.scrollY;
+        $('body').addClass('disable-scroll').data('position', pagePosition).css('top', -pagePosition + 'px');
+    };
 
     const enableScroll = () => {
-        let pagePosition = parseInt(document.body.dataset.position, 10);
-        document.body.style.top = 'auto';
-        document.body.classList.remove('disable-scroll');
+        const pagePosition = parseInt($('body').data('position'), 10);
+        $('body').css('top', 'auto').removeClass('disable-scroll');
         window.scroll({ top: pagePosition, left: 0 });
-        document.body.removeAttribute('data-position');
-    }
+        $('body').removeData('position');
+    };
 
     const openModalDesktop = () => {
-        modal.classList.add('is-open');
+        modal.addClass('is-open');
         disableScroll();
-    }
+    };
 
     const closeModal = () => {
-        modal.classList.remove('is-open');
-        disableScroll();
-    }
+        modal.removeClass('is-open');
+        enableScroll();
+    };
 
     const openModalMobile = () => {
-        modal.classList.add('is-open');
+        modal.addClass('is-open');
         disableScroll();
         setTimeout(() => {
-            modal.querySelector('.modal').classList.add('visible');
+            modal.find('.modal').addClass('visible');
         }, 100);
-    }
+    };
 
     const closeModalMobile = () => {
-        modal.querySelector('.modal').classList.remove('visible');
+        modal.find('.modal').removeClass('visible');
         setTimeout(() => {
-            modal.classList.remove('is-open');
+            modal.removeClass('is-open');
             enableScroll();
         }, 300);
-    }
+    };
 
-    const openModalDesktopL = () => {
-        modal.classList.add('is-open');
-        disableScroll();
-    }
-
-    const closeModalL = () => {
-        modal.classList.remove('is-open');
-        disableScroll();
-    }
-
-    const openModalMobileL = () => {
-        modal.classList.add('is-open');
-        disableScroll();
-        setTimeout(() => {
-            modal.querySelector('.modal').classList.add('visible');
-        }, 300);
-    }
-
-    const closeModalMobileL = () => {
-        modal.querySelector('.modal').classList.remove('visible');
-        setTimeout(() => {
-            modal.classList.remove('is-open');
-            enableScroll();
-        }, 400);
-    }
-
-    const handleBtnClick = (index) => {
-        openModalDesktopL();
-        openModalMobileL();
-    }
-
-    const handleBtnLClick = (index) => {
+    const handleBtnClick = () => {
         openModalDesktop();
         openModalMobile();
-    }
+    };
 
-    openBtn.addEventListener('click', () => {
+    const handleBtnLClick = () => {
+        openModalDesktop();
+        openModalMobile();
+    };
+
+    openBtn.on('click', () => {
         openModalDesktop();
         openModalMobile();
     });
 
-    openBtns.forEach((btn, index) => {
-        btn.addEventListener('click', () => {
+    openBtns.each(function(index) {
+        $(this).on('click', () => {
             handleBtnClick(index);
         });
     });
 
-    openBtnLs.forEach((btn, index) => {
-        btn.addEventListener('click', () => {
-            handleBtnLClick(index);
-        });
-    });
-
-    modal.addEventListener('click', (e) => {
-        if (e.target == modal) {
+    modal.on('click', (e) => {
+        if ($(e.target).hasClass('modals')) {
             closeModal();
-            closeModalL();
         }
     });
 
-    closeBtn.addEventListener('click', () => {
+    closeBtn.on('click', () => {
         closeModal();
-        closeModalL();
     });
 
-    mobileClose.addEventListener('swiped-down', () => {
+    mobileClose.on('swiped-down click', () => {
         closeModalMobile();
-        closeModalMobileL();
     });
 
-    mobileClose.addEventListener('click', () => {
-        closeModalMobile();
-        closeModalMobileL();
+    cryptobotb.on('click', () => {
+        $('.one_v_l').hide();
+        $('#i_pc').html("CryptoBot");
+        $('.c_form').show();
     });
 
-    cryptobotb.addEventListener('click', () => {
-        onev.style.display = 'none';
-        ch1.innerHTML = "CryptoBot";
-        form_c.style.display = 'block';
+    $('.acp').on('click', () => {
+        $('.one_v_l').show();
+        $('.pay_d').hide();
+        $('.c_form').hide();
     });
 
-    document.querySelector('.acp').addEventListener('click', () => {
-        onev.style.display = 'block';
-        document.querySelector('.pay_d').style.display = 'none';
-        form_c.style.display = 'none';
+    mobileClose.on('click', () => {
+        $('.pay_d').hide();
+        $('.c_form').hide();
     });
 
-    mobileClose.addEventListener('click', () => {
-        document.querySelector('.pay_d').style.display = 'none';
-        form_c.style.display = 'none';
-    });
-
-    cp_amount.addEventListener('input', () => {
-        let ncpa = parseFloat(cp_amount.value);
+    cp_amount.on('input', () => {
+        const ncpa = parseFloat(cp_amount.val());
         if (ncpa > 1 && ncpa < 1000) {
-            cryptobot.style.background = '#3390EC';
-            cryptobot.setAttribute('type', 'submit');
+            cryptobot.css('background', '#3390EC').attr('type', 'submit');
         } else {
-            cryptobot.style.background = '#707579';
-            cryptobot.setAttribute('type', 'button');
+            cryptobot.css('background', '#707579').attr('type', 'button');
         }
     });
 
-    openBtns.forEach((btn, index) => {
-        const wst = wsts[index].textContent;
-        const iwst = iws[index];
-        const twst = tws[index];
-        const pwst = pws[index];
-        const drow = drows[index];
-        if (wst.indexOf("CryptoBot") !== -1) {
-            btn.addEventListener('click', () => {
-                openModalDesktopL();
-                openModalMobileL();
+    openBtns.each(function(index) {
+        const wst = wsts.eq(index).text();
+        const iwst = iws.eq(index);
+        const twst = tws.eq(index);
+        const pwst = pws.eq(index);
+        const drow = drows.eq(index);
+        if (wst.includes("CryptoBot")) {
+            $(this).on('click', () => {
+                openModalDesktop();
+                openModalMobile();
             });
-            modal.addEventListener('click', (e) => {
-                if (e.target == modal) {
-                    closeModalL();
+            modal.on('click', (e) => {
+                if ($(e.target).hasClass('modals')) {
+                    closeModal();
                 }
             });
-            closeBtn.addEventListener('click', () => {
-                closeModalL();
+            closeBtn.on('click', () => {
+                closeModal();
             });
-            iwst.src = 'https://tptradix.github.io/OpenWorksUI/img/cash.png';
-            twst.innerHTML = 'Receive';
-            pwst.innerHTML = 'In proccess';
-            drow.style.display = 'inline-block';
+            iwst.attr('src', 'https://tptradix.github.io/OpenWorksUI/img/cash.png');
+            twst.html('Receive');
+            pwst.html('In proccess');
+            drow.css('display', 'inline-block');
         }
     });
 
-    iws.forEach((iwst, index) => {
-        iwst.addEventListener('click', () => {
-            const wst = wsts[index].textContent;
-            document.querySelector('.pay_d').style.display = 'grid';
-            form_c.style.display = 'none';
-            ch1.innerHTML = "Receive";
-            document.querySelector('.acpt_i').href = wst;
-            document.getElementById('del_f').value = document.querySelector('.at_id_' + (index + 1)).textContent.split(' ').join('');
-            console.log(document.getElementById('del_f').value);
+    iws.each(function(index) {
+        $(this).on('click', () => {
+            const wst = wsts.eq(index).text();
+            $('.pay_d').css('display', 'grid');
+            $('.c_form').hide();
+            $('#i_pc').html("Receive");
+            $('.acpt_i').attr('href', wst);
+            $('#del_f').val($('.at_id_' + (index + 1)).text().split(' ').join(''));
         });
     });
 
-    document.getElementById('re').addEventListener('click', () => {
+    $('#re').on('click', () => {
         window.location.reload();
     });
 
-}();
+    document.body.onselectstart = () => { return false; };
+});
